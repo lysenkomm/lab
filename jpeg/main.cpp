1387b4a1871ff778c9feee94408b2380d09605f8
asttoggie <asttoggie@gmail.com>
2016-10-28 23:42:04 +0300

    Инициализация сборки в NetBeans

--- jpeg/main.cpp
+++ jpeg/main.cpp
@@ -1,25 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Copyright 2016 AstToggie
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
  */
 
 /* 
  * File:   main.cpp
  * Author: asttoggie
- *
- * Created on October 28, 2016, 11:07 PM
+ * BMP24 to JPEG
+ * Created on October 11, 2016, 11:51 PM
  */
 
 #include <cstdlib>
+#include <stdio.h> // sprintf(..), fopen(..)
+#include <stdarg.h> //dprintf(..)
+#include <math.h> // sqrt(..), cos(..)
 
-using namespace std;
-
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    return 0;
+  return 0;
 }
 
+
+/*
+ char *JpgFileInName = "test.jpg";
+    FILE *fp;
+    fp = fopen(JpgFileInName, "rb");
+    if (fp == NULL)
+    {
+        dprintf("Cannot open jpg file: %s\n", JpgFileInName);
+        return 0;
+    } 
+ */
