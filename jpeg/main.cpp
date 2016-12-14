4999ad5c9d15597917cce0eafa93d62a9ad2d742
lysenkomm <lismari30@yandex.ru>
2016-12-14 23:34:52 +0200

    Update main.cpp

--- jpeg/main.cpp
+++ jpeg/main.cpp
@@ -22,7 +22,57 @@ limitations under the License.
 #include <stdio.h> // sprintf(..), fopen(..)
 #include <stdarg.h> //dprintf(..)
 #include <math.h> // sqrt(..), cos(..)
+#include "loadjpg.h" // ConvertJpgFile(..)
 
+#include "savejpg.h"    // SaveJpgFile(..)
+
+/***************************************************************************/
+/*                                                                         */
+/ FeedBack Data                                                           /
+/*                                                                         */
+/***************************************************************************/
+
+//Saving debug information to a log file
+void dprintf(const char *fmt, ...) 
+{
+ va_list parms;
+ char buf[256];
+
+ // Try to print in the allocated space.
+ va_start(parms, fmt);
+ vsprintf (buf, fmt, parms);
+ va_end(parms);
+
+ // Write the information out to a txt file
+ FILE *fp = fopen("output.txt", "a+");
+ fprintf(fp, "%s", buf);
+ fclose(fp);
+
+}// End dprintf(..)
+
+
+ 
+/***************************************************************************/
+/*                                                                         */
+/ Entry Point                                                             /
+/*                                                                         */
+/***************************************************************************/
+
+
+int __stdcall WinMain (HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
+{
+
+ // Create a jpg from a bmp
+ //SaveJpgFile("smiley.bmp", "ex.jpg");
+
+ // Create a bmp from a jpg
+ ConvertJpgFile("smiley.jpg", "smiley.bmp");
+
+
+ //ConvertJpgFile("cross.jpg", "cross.bmp");
+
+ return 0;
+}// End WinMain(..)
 int main(int argc, char** argv) 
 {    
     ///////////Convert RGB to YUB///////////////////
